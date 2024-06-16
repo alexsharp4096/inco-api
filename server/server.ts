1935ff29863a19e339a0aa0cd4fbc5c135c704cf
alexsharp4096 <alexsharp4096@gmail.com>
2024-06-16 14:38:57 -0500

    Copied from RodrigoBertotti/api-example-firebase-admin-nodejs, which has the setup done

--- server/server.ts
+++
@@ -1,5 +0,0 @@
-// equivalent of older: const express = require('express')
-import * as express from 'express';
-import { routes } from './routes/routes'; 
-const app = express();// Allow any method from any host and log requests
-app.use('/', routes);
